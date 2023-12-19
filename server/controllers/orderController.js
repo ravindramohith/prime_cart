488a23f6943a78a2325daf0264f88d30e4c42981
Ravindra Mohith P <ravindra_mohith@Ravindras-MacBook-Air.local>
2023-12-20 02:28:03 +0530

    (#45) Server: Orders :- Get Current User Orders

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -12,8 +12,14 @@ exports.placeOrder = catchAsync(async (req, res, next) => {
 });
 
 exports.getOrder = catchAsync(async (req, res, next) => {
-  const order = await Order.findById(req.params.id);
-  if (req.user._id.toString() !== order.user.toString())
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+
+  if (!order) return next(new ErrorHandler("Order not found", 404));
+
+  if (req.user._id.toString() !== order.user._id.toString())
     return next(
       new ErrorHandler("You are not authorized to view this order", 403)
     );
@@ -22,5 +28,14 @@ exports.getOrder = catchAsync(async (req, res, next) => {
     success: true,
     message: "Order retrieved successfully",
     order,
+  });
+});
+
+exports.getCurrentUserOrders = catchAsync(async (req, res, next) => {
+  const orders = await Order.find({ user: req.user._id });
+  res.status(200).json({
+    success: true,
+    message: "Orders retrieved successfully",
+    orders,
   });
 });
--- server/routers/orderRouter.js
+++ server/routers/orderRouter.js
@@ -1,9 +1,14 @@
-const { placeOrder, getOrder } = require("../controllers/orderController");
+const {
+  placeOrder,
+  getOrder,
+  getCurrentUserOrders,
+} = require("../controllers/orderController");
 const { checkAuth } = require("../middlewares/auth");
 
 const router = require("express").Router();
 
 router.route("/").post(checkAuth, placeOrder);
 router.route("/:id").get(checkAuth, getOrder);
+router.route("/my/orders").get(checkAuth, getCurrentUserOrders);
 
 module.exports = router;
