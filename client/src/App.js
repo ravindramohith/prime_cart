cf545e2045377413b802580a9177c9edb0143bab
Ravindra Mohith P <ravindra_mohith@Ravindras-MacBook-Air.local>
2023-12-20 04:39:59 +0530

    (#55) Client: Layout :- Added Header, Footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
+import "./App.css";
+import Footer from "./components/layout/Footer";
+import Header from "./components/layout/Header";
 function App() {
   return (
     <div className="App">
-      ESHOP
+      <Header />
+      <Footer />
     </div>
   );
 }
