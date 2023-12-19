5f9a14913faef5f141c16123dcf40020fc494796
Ravindra Mohith P <ravindra_mohith@Ravindras-MacBook-Air.local>
2023-12-19 16:21:06 +0530

    (#5) Server: Connecting with MongoDB using mongoose

--- server/server.js
+++ server/server.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 const app = express();
 
 if (process.env.NODE_ENV !== "PRODUCTION") {
   dotenv.config({ path: "server/config.env" });
 }
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then((connection) =>
+    console.log(`MongoDB connected with host: ${connection.connection.host}`)
+  )
+  .catch((e) => console.log(`MongoDB connect error: ${e}`));
 
 app.listen(process.env.PORT || 4000, () => {
   console.log(
