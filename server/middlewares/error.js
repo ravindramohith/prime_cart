649365cf4c802da04b1d5f73e00c4a615968b8be
Ravindra Mohith P <ravindra_mohith@Ravindras-MacBook-Air.local>
2023-12-19 18:00:00 +0530

    (#18) Server: Handling Invalid Mongoose ID Errors

--- server/middlewares/error.js
+++ server/middlewares/error.js
@@ -1,8 +1,15 @@
+const ErrorHandler = require("../utils/errorHandler");
+
 module.exports = (err, req, res, next) => {
   let error = {
     statusCode: err?.statusCode || 500,
     message: err?.message || "Internal Server Error",
   };
+
+  // Invalid Mongoose ID Error
+  if (err?.name === "CastError")
+    error = new ErrorHandler(`Resource not found. Invalid: ${err?.path}`, 404);
+
   if (process.env.NODE_ENV !== "PRODUCTION")
     res
       .status(error.statusCode)
