f1224f8be588717e03e3d63b30aae191fd91f953
Ravindra Mohith P <ravindra_mohith@Ravindras-MacBook-Air.local>
2023-12-19 16:17:51 +0530

    (#4) Server: Configuring with dotenv and setup npm scripts

--- server/server.js
+++ server/server.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const dotenv = require("dotenv");
 
 const app = express();
 
-app.listen(4000, () => {
-  console.log(`Server listening on port ${4000}`);
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  dotenv.config({ path: "server/config.env" });
+}
+
+app.listen(process.env.PORT || 4000, () => {
+  console.log(
+    `Server listening on port ${process.env.PORT || 4000} | Environment: ${
+      process.env.NODE_ENV
+    }`
+  );
 });
