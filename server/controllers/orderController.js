c8a30abbf31ddaf46423025a8afe8a1afe605102
Ravindra Mohith P <ravindra_mohith@Ravindras-MacBook-Air.local>
2023-12-20 02:19:06 +0530

    (#44) Server: Orders :- Get an Order

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,11 +1,26 @@
 const Order = require("../models/Order");
 const catchAsync = require("../utils/catchAsync");
+const ErrorHandler = require("../utils/errorHandler");
 
 exports.placeOrder = catchAsync(async (req, res, next) => {
   const order = await Order.create({ ...req.body, user: req.user._id });
   res.status(201).json({
     success: true,
     message: "Order placed successfully",
+    order,
+  });
+});
+
+exports.getOrder = catchAsync(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+  if (req.user._id.toString() !== order.user.toString())
+    return next(
+      new ErrorHandler("You are not authorized to view this order", 403)
+    );
+
+  res.status(200).json({
+    success: true,
+    message: "Order retrieved successfully",
     order,
   });
 });
--- server/routers/orderRouter.js
+++ server/routers/orderRouter.js
@@ -1,8 +1,9 @@
-const { placeOrder } = require("../controllers/orderController");
+const { placeOrder, getOrder } = require("../controllers/orderController");
 const { checkAuth } = require("../middlewares/auth");
 
 const router = require("express").Router();
 
 router.route("/").post(checkAuth, placeOrder);
+router.route("/:id").get(checkAuth, getOrder);
 
 module.exports = router;
