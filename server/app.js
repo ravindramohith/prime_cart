85fa31ba074b344fe75a26b0a0d47ef3e9b8d787
Ravindra Mohith P <ravindra_mohith@Ravindras-MacBook-Air.local>
2023-12-21 16:00:17 +0530

    (#62) Server: App :- Added Morgan middleware for logging APIs

--- server/app.js
+++ server/app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
 
 // body parser
 app.use(express.json());
 
 //cookie parser
 app.use(cookieParser());
+
+// Api Logging Middleware
+if (process.env.NODE_ENV !== "PRODUCTION") app.use(morgan("tiny"));
 
 // Routers
 app.use("/api/products", require("./routers/productRouter"));
