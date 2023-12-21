863ead59e9e5644fff38f54f8717adf973cc3454
Ravindra Mohith P <ravindra_mohith@Ravindras-MacBook-Air.local>
2023-12-21 16:17:46 +0530

    (#64) Client: Redux :- Loader and Cache

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,29 +2,30 @@ import React from 'react'
 import MetaData from './layout/MetaData'
 import { useGetProductsQuery } from '../redux/api/product'
 import ProductItem from './product/ProductItem'
+import Loader from './layout/Loader'
 
 const Home = () => {
-    const { data, isLoading } = useGetProductsQuery()
-    console.log(data)
+    const { data, isLoading } = useGetProductsQuery();
     return (
-        <>
-            <MetaData title={"Buy Best Products Online"} />
-            <div className="container">
-                <div className="row">
-                    <div className="col-6 col-md-12">
-                        <h1 id="products_heading" className="text-secondary">Latest Products</h1>
+        isLoading ? <Loader /> :
+            <>
+                <MetaData title={"Buy Best Products Online"} />
+                <div className="container">
+                    <div className="row">
+                        <div className="col-6 col-md-12">
+                            <h1 id="products_heading" className="text-secondary">Latest Products</h1>
 
-                        <section id="products" className="mt-5">
-                            <div className="row">
-                                {data?.data?.map((product, i) => (
-                                    <ProductItem key={i} product={product} />
-                                ))}
-                            </div>
-                        </section>
+                            <section id="products" className="mt-5">
+                                <div className="row">
+                                    {data?.data?.map((product, i) => (
+                                        <ProductItem key={i} product={product} />
+                                    ))}
+                                </div>
+                            </section>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </>
+            </>
     )
 }
 
