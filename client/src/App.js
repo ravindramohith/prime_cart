1712d199b72c629044081a0b2f30a28008a88901
Ravindra Mohith P <ravindra_mohith@Ravindras-MacBook-Air.local>
2023-12-21 16:23:55 +0530

    (#65) Client: Toast :- Adding React Hot Toast

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,12 @@ import Home from "./components/Home";
 import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 function App() {
   return (
     <Router>
       <div className="App">
+        <Toaster position="top-center" />
         <Header />
 
         <div className="container">
@@ -14,7 +16,7 @@ function App() {
             <Route path="/" element={<Home />} />
           </Routes>
         </div>
-        
+
         <Footer />
       </div>
     </Router>
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -3,9 +3,15 @@ import MetaData from './layout/MetaData'
 import { useGetProductsQuery } from '../redux/api/product'
 import ProductItem from './product/ProductItem'
 import Loader from './layout/Loader'
+import toast from 'react-hot-toast'
 
 const Home = () => {
-    const { data, isLoading } = useGetProductsQuery();
+    const { data, isLoading, error, isError } = useGetProductsQuery();
+    React.useEffect(() => {
+        if (isError) {
+            toast.error(error?.data?.message)
+        }
+    }, [isError])
     return (
         isLoading ? <Loader /> :
             <>
