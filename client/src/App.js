bad0a054b8d8bc365754e63620113972578e6422
Ravindra Mohith P <ravindra_mohith@Ravindras-MacBook-Air.local>
2023-12-21 08:54:16 +0530

    (#56) Client: Home :- Added Home Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import Home from "./components/Home";
 import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="container">
+        <Home />
+      </div>
       <Footer />
     </div>
   );
